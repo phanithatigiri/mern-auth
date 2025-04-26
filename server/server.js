0397ce91a9e3576037530c3b1d9907f935580d0a
phanithatigiri <phanithatigiri123@gmail.com>
2025-04-26 16:42:19 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import router from './routes/auth.js'
 const app = express()
 const PORT = process.env.PORT || 5000
 app.use(express.json())
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
 app.use(cookieParser())
 
 
