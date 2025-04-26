cf17b559ed11f250b84c63db5cd88ca8db5d7dd9
phanithatigiri <phanithatigiri123@gmail.com>
2025-04-26 17:38:05 +0530

    initial commit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -131,7 +131,7 @@ export const logout = async (req,res)=>{
       {
         httpOnly:true,
             secure:process.env.NODE_ENV === 'production',
-             sameSite:"strict",
+             sameSite:process.env.NODE_ENV === 'production' ? "none":"lax",
             maxAge:7 * 24 * 60 * 60 * 1000
       }
     )
