f7c867572af7f97cef32fd8e135a389a6f822e28
phanithatigiri <phanithatigiri123@gmail.com>
2025-04-26 17:27:06 +0530

    initial commit

--- server/utils/generateTokenandcookie.js
+++ server/utils/generateTokenandcookie.js
@@ -8,7 +8,7 @@ const generateTokenandcookie = (res,userId) =>{
         res.cookie("token",token,{
             httpOnly:true,
             secure:process.env.NODE_ENV === 'production',
-             sameSite:"lax",
+             sameSite:process.env.NODE_ENV === 'production' ? "none":"lax",
             maxAge:7 * 24 * 60 * 60 * 1000
         })
         return token
